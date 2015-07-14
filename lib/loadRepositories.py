1917f40741acee5afc1c6b61f5a25c50e3d27ff9
davisd <daniel.davis@excella.com>
2015-07-14 10:28:57 -0400

    clone unitybox to the workspace;
    needed access to common modules and roles that are provided
    in unitybox, so including it in the cloning of the workspace

--- lib/loadRepositories.py
+++ lib/loadRepositories.py
@@ -4,9 +4,6 @@ import os
 import sys
 import subprocess
 
-def get_repo_dependencies(data):
-    return data.get("repositories") or data.get("public_repositories") or data.get("private_repositories", [])
-
 
 def print_msg(msg):
     cmd = 'echo {0}:'.format(msg)
@@ -15,6 +12,8 @@ def print_msg(msg):
 
 def clone_repo(pinned_url, is_pinned, workspace_dir, path=None):
     url, version = pinned_url.split('@') if '@' in pinned_url else [pinned_url, None]
+    if pinned_url and not version:
+        raise Exception("You must explicitly pin a version (e.g. append `@v1.0`) to {0}".format(url))
     repo_parent_dir = os.path.join(workspace_dir, path) if path else workspace_dir
     repo_name = url.split('/')[4].replace(".git", "")
     repo_dir = os.path.join(repo_parent_dir, repo_name)
@@ -44,7 +43,8 @@ def clone_repo(pinned_url, is_pinned, workspace_dir, path=None):
     return repo_name
 
 
-def load_repo_data(repo_name, workspace_dir):
+def read_repo_data(repo_name, workspace_dir):
+    """ Return repositories.yml data from an already-cloned repository """
     repo_path = os.path.join(workspace_dir, repo_name)
     repo_data_path = os.path.join(repo_path, 'ansible/group_vars/all/repositories.yml')
     try:
@@ -57,20 +57,35 @@ def load_repo_data(repo_name, workspace_dir):
     return repo_data
 
 
-def load_repositories(repo_name, is_pinned, workspace_dir):
-    repo_data = load_repo_data(repo_name, workspace_dir)
+def build_repo_data(repo_name, is_pinned, workspace_dir):
+    """ Return merged repositories.yml data from both public and private repos """
+    repo_data = read_repo_data(repo_name, workspace_dir)
     proj_url = repo_data.get('project_repository')
-    repo_dependencies = get_repo_dependencies(repo_data)
     if proj_url:
         proj_name = clone_repo(proj_url, is_pinned, workspace_dir)
-        proj_data = load_repo_data(proj_name, workspace_dir)
-        repo_dependencies += get_repo_dependencies(proj_data)
+        proj_data = read_repo_data(proj_name, workspace_dir)
+        proj_data.update(repo_data)
+        repo_data = proj_data
+        if "private_repositories" in repo_data or "public_repositores" in repo_data:
+            repo_data['repositories'] = repo_data.get("private_repositories", []) + repo_data.get("public_repositores", [])
     else:
         proj_name = repo_name
 
-    for pinned_url in repo_dependencies:
+    return repo_data, proj_name
+
+
+def load_repositories(repo_name, is_pinned, workspace_dir):
+    repo_data, proj_name = build_repo_data(repo_name, is_pinned, workspace_dir)
+    
+    for pinned_url in repo_data['repositories']:
         clone_repo(pinned_url, is_pinned, workspace_dir, proj_name+'-repos')
 
+    try:
+        unitybox_repository = repo_data['unitybox_repository']
+    except KeyError: 
+        raise Exception("repositories.yml must contain an entry for unitybox_repository: e.g. `unitybox_repository: https://github.com/cfpb/Unitybox.git@xxx`")
+
+    clone_repo(unitybox_repository, True, workspace_dir)
 
 if __name__ == "__main__":
     repo_name = sys.argv[1]
