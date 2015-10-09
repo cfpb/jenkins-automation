7ff42f71125634c060f8e1320b47d8e95f1dbba4
Irina Muchnik <irina.muchnik@cfpb.gov>
2015-10-09 15:50:06 -0400

    Extending flow job sample

--- jobs/SampleBuildFlow.groovy
+++ jobs/SampleBuildFlow.groovy
@@ -1,8 +1,15 @@
 import FlowJobBuilder
 
-new FlowJobBuilder(
+def oahMaster= new FlowJobBuilder(
         name: 'GeneratedFlowJob',
         description: 'this our first stab at it',
         jobs:['job1', 'job2']
 ).build(this);
+
+oahMaster.with{
+  logRotator{
+      numToKeep(365)
+  }
+
+}
 
