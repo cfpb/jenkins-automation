95b414616b0c4375fc735066e966cc9d51a62e11
Irina Muchnik <irina.muchnik@cfpb.gov>
2015-10-29 11:28:53 -0400

    valid test

--- src/test/groovy/cfGovMigrationsTests.groovy
+++ src/test/groovy/cfGovMigrationsTests.groovy
@@ -2,54 +2,29 @@
  * Created by muchniki on 10/27/15.
  */
 import javaposse.jobdsl.dsl.*
-import javaposse.jobdsl.dsl.DslContext
 import spock.lang.Specification
 
-@Mixin(SpecUtilsMixin)
 class cfGovMigrationsTests extends Specification {
-    JobParent jobParent = createJobParent()
-    FlowJobBuilder builder
-
-    def setup() {
-        builder = new FlowJobBuilder(
-                name: 'test-job',
-                description: 'testing'
-        )
-    }
-
-//    void 'test XML output'() {
-//        when:
-//        jobParent
-//        Job job = builder.build(jobParent)
-//        print job
-//        then:
-//        job.name == builder.name
-//
-//        with(job.node) {
-//            print it
-//            name() == 'project'
-//            triggers.'hudson.triggers.SCMTrigger'.spec.text() == '*/5 * * * *'
-//            !publishers.'hudson.tasks.Mailer'.recipients
-//        }
-//    }
 
     void 'test cf.gov flow build job'() {
 
         given:
         JobManagement jm = new MemoryJobManagement()
 
-        Closure testjob = { new File('src/test/migrated-jobs/prod-cfgov-buildFlow.groovy').text.toString() }
-        def ch = new ContextHelper()
+        def testjob =  new File('src/test/migrated-jobs/prod-cfgov-buildFlow.groovy').text
+        def expected = new File('src/test/resources/prod-cf-gov-build-flow.xml').text
 
         when:
-//        //def result=DslScriptLoader.runDslEngine testjob.text, jm
-//       def result=ch.executeInContext(testjob, DslFactory.hashCode())
-        testjob.resolveStrategy = Closure.DELEGATE_FIRST
-        def result=testjob.call()
+
+        ScriptRequest scriptRequest = new ScriptRequest(null, testjob, new File('.').toURI().toURL())
+        DslScriptLoader.runDslEngine(scriptRequest, jm)
+
+        def result = jm.savedConfigs.collect { [name: it.key, xml: it.value] }
+        result += jm.savedViews.collect { [name: it.key, xml: it.value] }
 
         then:
 
-        println result
+        result[0].xml==expected
 
     }
 
