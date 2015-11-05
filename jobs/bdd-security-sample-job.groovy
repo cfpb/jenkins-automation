da642930d3f99ba87b48661bd3870824c1c77b53
Irina Muchnik <irina.muchnik@cfpb.gov>
2015-11-05 15:05:05 -0500

    First stab at bdd secuirty builder

--- jobs/bdd-security-sample-job.groovy
+++ jobs/bdd-security-sample-job.groovy
@@ -2,7 +2,7 @@ import BddSecurityJobBuilder
 
 def projectName ='foo'
 new BddSecurityJobBuilder(
-        name: "${projectName}-bdd-security-job",
+        name: "${projectName}bdd_security_job",
         description: this.description,
         baseUrl: "http://google.com"
 ).build(this);
