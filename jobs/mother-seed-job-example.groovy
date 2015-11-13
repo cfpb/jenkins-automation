f022b27dfd0bcf75d9d71680cdc0bbf51ee5b610
Irina Muchnik <irina.muchnik@cfpb.gov>
2015-11-13 10:45:33 -0500

    this should work for sure

--- jobs/mother-seed-job-example.groovy
+++ jobs/mother-seed-job-example.groovy
@@ -3,17 +3,17 @@ import jenkins.automation.utils.ScmUtils
 
 //def reposToAutomate = RepositoryYamlParser.parseRepositories('some_yaml_file')
 def reposToAutomate = [
-        [projectName: "OAH", url: "https://github.cfpb.gov/muchniki/oah-jenkins-automation"],
-        [projectName: "automation", url: 'https://github.com/cfpb/jenkins-automation']
+        [projectName: "OAH", url: "https://github.cfpb.gov/muchniki/oah-jenkins-automation"]
 
 ]
-
+def reposToInclude =reposToAutomate
+reposToInclude<< [projectName: "automation", url: 'https://github.com/cfpb/jenkins-automation']
 
 reposToAutomate.each { project ->
     job(project.projectName + 'SeedJob') {
 
         multiscm {
-            ScmUtils.project_repos(delegate, reposToAutomate, false)
+            ScmUtils.project_repos(delegate, reposToInclude, false)
         }
 
         triggers {
