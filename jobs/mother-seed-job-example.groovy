eb94247fc4fe41a3db11059fc11d59810223373a
Irina Muchnik <irina.muchnik@cfpb.gov>
2015-11-18 12:42:36 -0500

    Fixing quirks in the mother jobs Adding View creation to correspond to the project Cleanup

--- jobs/mother-seed-job-example.groovy
+++ jobs/mother-seed-job-example.groovy
@@ -1,15 +1,35 @@
 import jenkins.automation.utils.ScmUtils
 
+import java.lang.reflect.Array
+
 
 //def reposToAutomate = RepositoryYamlParser.parseRepositories('some_yaml_file')
 def reposToAutomate = [
         [projectName: "OAH", url: "https://github.cfpb.gov/muchniki/oah-jenkins-automation"]
 
 ]
-def reposToInclude =reposToAutomate
+def reposToInclude  = reposToAutomate.collect()
+
+
 reposToInclude<< [projectName: "automation", url: 'https://github.com/imuchnik/jenkins-automation']
 
 reposToAutomate.each { project ->
+    listView(project.projectName) {
+        columns {
+            status()
+            weather()
+            name()
+            lastSuccess()
+            lastFailure()
+            lastDuration()
+            buildButton()
+        }
+        filterBuildQueue()
+        filterExecutors()
+        jobs {
+            regex(/(?i)(${project.projectName}.*)/)
+        }
+    }
     job(project.projectName + 'SeedJob') {
 
         multiscm {
