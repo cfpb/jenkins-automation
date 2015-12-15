d1b56163b20b2e85db02c14793b43f53d3808ab7
Irina Muchnik <irina.muchnik@cfpb.gov>
2015-12-15 15:50:40 -0500

    Adding build automationin to this repo

---
+++ jobs/jac.groovy
@@ -0,0 +1,30 @@
+job('generate docs') {
+    scm {
+        git {
+            remote {
+                name('origin')
+                url("git@github.com:cfpb/jenkins-automation.git")
+            }
+        }
+    }
+
+    steps{
+        gradle('groovydoc')
+        useWrapper()
+        shell("""
+            git add -A
+            git commit -m"Jenkins autogenerate docs
+       """)
+    }
+    triggers {
+        scm 'H/5 * * * *'
+    }
+    publishers {
+        git {
+            pushOnlyIfSuccess()
+            pushMerge(false)
+            forcePush(true)
+            branch(origin,'gh-pages')
+        }
+    }
+}
