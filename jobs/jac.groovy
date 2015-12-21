127a548cfcd9d2c96147941c6c29cc38e279bd7f
Irina Muchnik <irina.muchnik@cfpb.gov>
2015-12-21 17:00:56 -0500

    Fix generate docs job

--- jobs/jac.groovy
+++ jobs/jac.groovy
@@ -5,6 +5,8 @@ job('generate docs') {
                 name('origin')
                 url("https://github.com/cfpb/jenkins-automation")
                 credentials('009c8c9d-3cf5-4b2a-89f3-286977cabddf')
+                clean()
+                wipeOutWorkspace()
             }
         }
     }
@@ -15,6 +17,7 @@ job('generate docs') {
             useWrapper()
         }
         shell("""
+            git checkout gh-pages
             git add -A
             git commit -m'Jenkins autogenerate docs'
        """)
