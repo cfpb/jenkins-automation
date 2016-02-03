e4e6995d5156ff2ee26bebb0f4f324ee89c1f0a4
Irina Muchnik <irina.muchnik@cfpb.gov>
2016-02-03 16:14:12 -0500

    Adding docs for shallow clone

--- src/main/groovy/jenkins/automation/utils/ScmUtils.groovy
+++ src/main/groovy/jenkins/automation/utils/ScmUtils.groovy
@@ -2,12 +2,19 @@ package jenkins.automation.utils
 
 /**
  * Utility class to provide nicer, terser DSL for common tasks
+ *
  *  @see <a href="https://github.com/imuchnik/jenkins-automation/blob/gh-pages/docs/examples.md#using-multiscm-utility" target="_blank">Using Utility Class example</a>
 
  */
 class ScmUtils {
 
-
+/***
+ *
+ * Utility method to create a multiScm block from a List of repos
+ * @param repos List of repo maps
+ * @param use_versions flag to check out the repo at a specific tag. The tag is parsed out from url property appended after @ sign.
+ * each repo object must include url property and optionally sub_directory(checkout subdirectory, shallow(shallow clone) and disable_submodule properties.
+ */
     static void project_repos(context, repos, use_versions = true) {
         Boolean disable_submodule = false
         context.with {
@@ -25,7 +32,7 @@ class ScmUtils {
                         url(parsed_url)
                     }
                     if (use_versions && version!=null) {
-                        branch("*/tags/$version")
+                        branch "*/tags/$version"
                     }
                     else {
                         branch "master"
