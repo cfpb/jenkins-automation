4f55a95f694436f856bd6d47d4b75132b218a2a1
Irina Muchnik <irina.muchnik@cfpb.gov>
2016-02-05 11:42:46 -0500

    even more docs

--- src/main/groovy/jenkins/automation/builders/JsJobBuilder.groovy
+++ src/main/groovy/jenkins/automation/builders/JsJobBuilder.groovy
@@ -18,7 +18,8 @@ import jenkins.automation.utils.ScmUtils
 
  * @param emails  List or String of notification email addresses
  * @param repos List of repos to watch
- * @param use_versions flag to check out the repo at a specific tag. The tag is parsed out from url property appended after '@' sign.
+ * @param use_versions flag to check out the repo at a specific tag. Applies only to MultiScm block.
+ *          The tag is parsed out from url property appended after '@' sign.
 
  * <p>
  *
