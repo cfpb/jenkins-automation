097395862f56910644f98b33599a38b98d897550
Irina Muchnik <irina.muchnik@cfpb.gov>
2016-07-12 10:27:59 -0400

    TYPO

--- src/main/groovy/jenkins/automation/utils/pluginUtils.groovy
+++ src/main/groovy/jenkins/automation/utils/pluginUtils.groovy
@@ -8,7 +8,7 @@ class PluginUtils {
 /**
  *  Utility function for adding newrelic notifier plugin
  * @param context Publishers in this case, passed as a delegate
- * @param apiKey newRelic api key
+ * @param api_key newRelic api key
  * @param applicationId Application Id
  * @param jobName job Name
  * @param changeLog Changelog messages
@@ -19,12 +19,12 @@ class PluginUtils {
  * @see <a href="https://github.com/cfpb/jenkins-automation/blob/gh-pages/docs/examples.md#plugin-utils" target="_blank">Plugin utils</a>
  */
 
-    static void addNewRelicSupport(context, apiKey, application_Id, jobName, changeLog, userId, rev) {
+    static void addNewRelicSupport(context, api_key, application_Id, jobName, changeLog, userId, rev) {
 
         context.configure { Node project ->
             project / publishers << 'jenkinsci.plugins.newrelicnotifier.NewRelicDeploymentNotifier'(plugin: "newrelic-deployment-notifier@1.3") {
                 notifications {
-                    goo apiKey
+                    apiKey api_key
                     applicationId application_Id
                     description jobName
                     revision rev
--- src/test/groovy/my_job_test.groovy
+++ src/test/groovy/my_job_test.groovy
@@ -15,10 +15,11 @@ class my_job_test extends Specification {
         when:
         ScriptRequest scriptRequest = new ScriptRequest(null, testjob, new File('.').toURI().toURL())
         DslScriptLoader.runDslEngine(scriptRequest, jm)
+        def result = jm.savedConfigs.collect { [name: it.key, xml: it.value] }
 
 
         then:
-
+        print result
         noExceptionThrown()
 
     }
