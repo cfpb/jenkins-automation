d4a92102858705cceab680baece3611b7f5997ad
Vic Kumar <vickumar@gmail.com>
2016-07-29 14:38:34 -0400

    Add example job for sauce connect builder.

---
+++ example-jobs/sauce_connect_job.groovy
@@ -0,0 +1,9 @@
+import jenkins.automation.builders.SauceConnectJobBuilder
+
+def projectName ='foo'
+new SauceConnectJobBuilder(
+        name: "${projectName}-browser-tests",
+        description: "Sample sauce connect job",
+        emails: ['email1@server1.com', 'email2@server2.com'],
+        sauceCredentialId: '1234'
+).build(this);
