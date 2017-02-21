5ba103ddfbe802598ba2d6d0ce27fc79f2c7ca4f
Irina Muchnik <irina.muchnik@cfpb.gov>
2017-02-21 16:16:32 -0500

    Fixing deprecated worskspace clean syntax

--- jenkins-bootstrap.groovy
+++ jenkins-bootstrap.groovy
@@ -1,38 +1,42 @@
 job('dsl-master') {
-  description('This job rebuilds dsl-project-builder whenever jenkins-automation or jenkins-automation-private changes.')
-  
-  multiscm {
-    git {
-      remote {
-        url('https://github.com/Ooblioob/jenkins-automation')
-      }
-      branch('*/master')
-      relativeTargetDir('jenkins-automation')
-    }
-    git {
-      remote {
-        url(JENKINS_AUTOMATION_PRIVATE_GIT_URL)
-      }
-      branch('*/master')
-      relativeTargetDir('jenkins-automation-private')
-    }
-  }
+    description('This job rebuilds dsl-project-builder whenever jenkins-automation or jenkins-automation-private changes.')
 
-  triggers {
-    scm('H/3 * * * *')
-  }
+    multiscm {
+        git {
+            remote {
+                url('https://github.com/Ooblioob/jenkins-automation')
+            }
+            branch('*/master')
+            extensions {
+                relativeTargetDir('jenkins-automation')
+            }
+        }
+        git {
+            remote {
+                url(JENKINS_AUTOMATION_PRIVATE_GIT_URL)
+            }
+            branch('*/master')
+            extensions {
+                relativeTargetDir('jenkins-automation-private')
+            }
+        }
+    }
 
-  steps {
-    shell('''
+    triggers {
+        scm('H/3 * * * *')
+    }
+
+    steps {
+        shell('''
     ./jenkins-automation/lib/getProjectRepos.py
     ''')
-    dsl {
-      external(['jenkins-automation/jenkins-automation.groovy'])
-      removeAction('DISABLE')
+        dsl {
+            external(['jenkins-automation/jenkins-automation.groovy'])
+            removeAction('DISABLE')
+        }
     }
-  }
 
-  wrappers {
-    colorizeOutput()
-  }
+    wrappers {
+        colorizeOutput()
+    }
 }
--- jobs/jac.groovy
+++ jobs/jac.groovy
@@ -6,7 +6,9 @@ job('generate docs') {
                 branch('master')
                 url("https://github.com/cfpb/jenkins-automation")
                 credentials('009c8c9d-3cf5-4b2a-89f3-286977cabddf')
-                clean()
+            }
+            extensions {
+                cleanBeforeCheckout()
                 wipeOutWorkspace()
             }
         }
