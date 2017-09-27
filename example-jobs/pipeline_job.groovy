9c43ad8e17c3e0aae93e500e3bb894b09dfd5efe
Irina Muchnik <irina.muchnik@cfpb.gov>
2017-09-27 14:57:55 -0400

    WIP pipelines

---
+++ example-jobs/pipeline_job.groovy
@@ -0,0 +1,52 @@
+import jenkins.automation.builders.PipelineJobBuilder
+
+def script = """
+    node {
+        stage('First stage') {
+            build job: 'Job 1',
+            parameters: [
+                [\$class: 'StringParameterValue', name: 'foo', value: 'bar'],
+            ]
+        }
+    }
+"""
+
+def pipelineJob = new PipelineJobBuilder(
+    name: 'Pipeline builder',
+    description: 'This is a simple pipeline job',
+    pipelineScript: script,
+    emails: ['jane@example.com', 'joe@example.com']
+).build(this);
+
+pipelineJob.with {
+    logRotator {
+        numToKeep(365)
+    }
+}
+
+
+// ****************************************************************************
+
+
+def pipelineJobStages = new PipelineJobBuilder(
+    name: 'Pipeline builder',
+    description: 'This is a simple pipeline job',
+    stages: [
+        [
+            stageName: 'First stage',
+            jobName: 'Job 1',
+            parameters: "[[\$class: 'StringParameterValue', name: 'foo', value: 'bar']]"
+        ],
+        [
+            stageName: 'Second stage',
+            jobName: 'Job 2',
+        ]
+    ],
+    emails: ['jane@example.com', 'joe@example.com']
+).build(this);
+
+pipelineJobStages.with {
+    logRotator {
+        numToKeep(365)
+    }
+}
