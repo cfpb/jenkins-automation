280a8a1fc776e810c20e924fc049b9ddad2f9aa8
Orlando Soto <Orlando.Soto-Pastor@cfpb.gov>
2018-01-09 15:11:02 -0500

    fix endpoint

--- example-jobs/mb_pipeline_job.groovy
+++ example-jobs/mb_pipeline_job.groovy
@@ -7,7 +7,7 @@ new MultibranchPipelineJobBuilder(
         ghOrganizationName: "testprbuilder",
         ghRepositoryName: "pr_tester",
         ghScanCredentialsId: "8fbdbaa0-d5ff-4acb-9e8f-27e49b77048b",
-        ghEndpoint: "https://github.cfpb.gov/api/v3",
+        ghEndpoint: "https://github.example.com/api/v3/",
         branchSourceGit: true,
         ghRemote: "https://github.com/OrlandoSoto/orlando-shared-libraries.git",
         ghCredentialsId: "009c8c9d-3cf5-4b2a-89f3-286977cabddf",
