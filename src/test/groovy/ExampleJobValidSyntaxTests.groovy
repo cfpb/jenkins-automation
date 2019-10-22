a964ef69fb4b1d75b837ababd3a4e603f65e6dec
Marc Esher <marc.esher@cfpb.gov>
2019-10-22 05:57:28 -0400

    Update job dsl (#145)
    * Update job-dsl dependency to latest
    
    We're now running the latest job-dsl-plugin on our Jenkins servers. This
    gets our automation helpers up to that version
    
    * Update job-dsl to latest; fix test to accomodate
    
    This updates to the latest version of job-dsl.
    
    I also brought over the syntax tests that Irina built for cf.gov, and adapted them for our example jobs. This is a vast improvement over the original test.
    
    * fix formatting

---
+++ src/test/groovy/ExampleJobValidSyntaxTests.groovy
@@ -0,0 +1,39 @@
+import groovy.io.FileType
+import javaposse.jobdsl.dsl.*
+import spock.lang.*
+
+class my_job_test extends Specification {
+    void 'test dev and prod jobs do not fail'() {
+
+        given:
+        JobManagement jm = new MemoryJobManagement()
+        DslScriptLoader scriptLoader = new DslScriptLoader(jm)
+
+        def params = jm.getParameters()
+
+        List<File> files = []
+
+        new File('example-jobs').eachFileRecurse(FileType.FILES) {
+            if (it.name.endsWith('.groovy')) {
+                files << it
+            }
+        }
+
+        when:
+        ['dev', 'stage', 'prod'].each { environment ->
+            params['JAC_ENVIRONMENT'] = environment
+
+            println "\nTesting jobs for with environment = $environment \n"
+
+            files.each { file ->
+                println file
+                scriptLoader.runScript(file.text)
+            }
+        }
+
+        then:
+
+        assert true
+        noExceptionThrown()
+    }
+}
