1ed2509fb019260223b12e84ff52bf7f58202e62
Marc Esher <marc.esher@cfpb.gov>
2020-07-30 15:23:55 -0400

    https on site_monitor examples

--- example-jobs/site_monitor_job.groovy
+++ example-jobs/site_monitor_job.groovy
@@ -5,7 +5,7 @@ new SiteMonitorJobBuilder(
         name: "my-site-pulse-check",
         description: "Sample url pulse check job",
         cronSchedule: "@daily",
-        urls: ["http://google.com","http://yourethemannowdog.ytmnd.com/"]
+        urls: ["https://google.com","https://yourethemannowdog.ytmnd.com/"]
 ).build(this);
 
 
@@ -13,6 +13,6 @@ new SiteMonitorJobBuilder(
         name: "my-site-pulse-check-without-schedule",
         description: "Sample url pulse check job without a schedule",
         cronSchedule: "",
-        urls: ["http://google.com","http://yourethemannowdog.ytmnd.com/"]
+        urls: ["https://google.com","https://yourethemannowdog.ytmnd.com/"]
 ).build(this);
 
